f68dd58d9d09f953475f4af8508481981b1b839d
shahin <muh465838shahinsha@gmail.com>
2025-07-19 09:32:00 +0530

    latest changes  july 19

--- week3/Practical/3arr.py
+++ week3/Practical/3arr.py
@@ -13,3 +13,19 @@ arr2=[24,15,9,42,63]
 arr3=[25,12,36,85,96]
 
 print(larg(arr1,arr2,arr3))
+
+def larg(*arr):
+    lg=arr[0][0]
+    max_arr=arr[0]
+    for sub in arr:
+        for i in sub:
+            if i>lg:
+                lg=i
+                max_arr=sub
+    return lg,max_arr
+    
+arr1=[10,28,37,87,6,41]
+arr2=[24,15,900,42,63]
+arr3=[25,12,36,85,96]
+
+print(larg(arr1,arr2,arr3))
